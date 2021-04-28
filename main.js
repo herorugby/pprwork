6afaf4d4c32d7a204634c06b266481c9f06ad422
herorugby <hero.rugby1015@gmail.com>
2021-04-28 21:27:28 +0900

    ppr works

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 // jqueryなど
 
+// drawer.js
 $(document).ready(function () {
     $('.drawer').drawer();
+});
+
+// swiper.js
+let swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
 });
